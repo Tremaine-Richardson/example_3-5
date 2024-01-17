6f9fd12fdc33342f802ad9a2c6361883c2200be5
Tremaine-Richardson <trey7836@gmail.com>
2024-01-17 20:15:22 +0000

    with driver seat welcoming

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 //=====[Libraries]=============================================================
 
-#include "mbed.h"
 #include "arm_book_lib.h"
+#include "mbed.h"
 
 //=====[Defines]===============================================================
 
-#define NUMBER_OF_SENSORS                           4
+#define NUMBER_OF_SENSORS 4
 
 //=====[Declaration and initialization of public global objects]===============
 
@@ -14,7 +14,6 @@ DigitalIn passengerSeat(D2);
 DigitalIn passengerBelt(D3);
 DigitalIn driverSeat(D4);
 DigitalIn driverBelt(D5);
-
 
 DigitalOut canStartLED(LED1);
 DigitalOut ignitionLED(LED2);
@@ -25,12 +24,12 @@ UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
 //=====[Declaration and initialization of public global variables]=============
 
-bool canStartState    = OFF;
+bool canStartState = OFF;
 bool ignitionPushed = OFF;
+bool prevDriverSeat = OFF;
 
-int allOn[NUMBER_OF_SENSORS] = { 1, 1, 1, 1 };
-int sensorsOn[NUMBER_OF_SENSORS] = { 0, 0, 0, 0 };
-
+int allOn[NUMBER_OF_SENSORS] = {1, 1, 1, 1};
+int sensorsOn[NUMBER_OF_SENSORS] = {0, 0, 0, 0};
 
 //=====[Declarations (prototypes) of public functions]=========================
 
@@ -46,99 +45,98 @@ bool areEqual();
 
 //=====[Main function, the program entry point after power on or reset]========
 
-int main()
-{
-    inputsInit();
-    outputsInit();
-    uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48);
-    while (ignitionPushed == OFF) {
-        sensorUpdate();
-        ignitionActivated();
-    }
+int main() {
+  inputsInit();
+  outputsInit();
+  while (ignitionPushed == OFF) {
+    sensorUpdate();
+    ignitionActivated();
+  }
 }
 
 //=====[Implementations of public functions]===================================
 
-void inputsInit()
-{
-    ignitionButton.mode(PullDown);
-    passengerSeat.mode(PullDown);
-    passengerBelt.mode(PullDown);
-    driverSeat.mode(PullDown);
-    driverBelt.mode(PullDown);
-    sirenPin.mode(OpenDrain);
-    sirenPin.input();
+void inputsInit() {
+  ignitionButton.mode(PullDown);
+  passengerSeat.mode(PullDown);
+  passengerBelt.mode(PullDown);
+  driverSeat.mode(PullDown);
+  driverBelt.mode(PullDown);
+  sirenPin.mode(OpenDrain);
+  sirenPin.input();
 }
 
-void outputsInit()
-{
-    canStartLED = OFF;
-    ignitionLED = OFF;
+void outputsInit() {
+  canStartLED = OFF;
+  ignitionLED = OFF;
 }
 
-void sensorUpdate()
-{   
-    sensorsOn[0] = passengerSeat;
-    sensorsOn[1] = passengerBelt;
-    sensorsOn[2] = driverSeat;
-    sensorsOn[3] = driverBelt;
-    if ( areEqual() ) {
-        canStartState = ON;
+void sensorUpdate() {
+  sensorsOn[0] = passengerSeat;
+  sensorsOn[1] = passengerBelt;
+  sensorsOn[2] = driverSeat;
+  sensorsOn[3] = driverBelt;
+  if ( prevDriverSeat == OFF) {
+      if ( driverSeat ) {
+            uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48);
+            prevDriverSeat = !prevDriverSeat;
+      }
+  }
+  else {
+      if ( !driverSeat ) {
+          prevDriverSeat = !prevDriverSeat;
+      }
+  }
+  if (areEqual()) {
+    canStartState = ON;
+  } else {
+    canStartState = OFF;
+  }
+  canStartLED = canStartState;
+}
+
+void ignitionActivated() {
+  if (ignitionButton) {
+    if (canStartState) {
+      ignitionLED = ON;
+      canStartLED = OFF;
+    } else {
+      sirenPin.output();
+      sirenPin = LOW;
     }
-    else {
-        canStartState = OFF;
-    }
-    canStartLED = canStartState;
-}
-
-void ignitionActivated()
-{   
-    if ( ignitionButton ) {
-        if ( canStartState ) {
-            ignitionLED = ON;
-            canStartLED = OFF;
-        }
-        else {
-            sirenPin.output();                                     
-            sirenPin = LOW; 
-        }
     uartEnd();
     ignitionPushed = ON;
-    }
+  }
 }
 
-void uartEnd()
-{
-    if ( canStartState ) {
-        uartUsb.write( "Engine Started\r\n", 16);
+void uartEnd() {
+  if (canStartState) {
+    uartUsb.write("Engine Started\r\n", 16);
+  } else {
+    uartUsb.write("Ignition inhibited for the following reasons:\r\n", 47);
+    if (sensorsOn[0] == 0) {
+      uartUsb.write("Passenger seat not occupied\r\n", 29);
     }
-    else {
-        uartUsb.write( "Ignition inhibited for the following reasons:\r\n", 47);
-        if ( sensorsOn[0] == 0 ) {
-            uartUsb.write( "Passenger seat not occupied\r\n", 29);
-        }
-        if ( sensorsOn[1] == 0) {
-            uartUsb.write( "Passenger seatbelt not fastened\r\n", 33);
-        }
-        if ( sensorsOn[2] == 0) {
-            uartUsb.write( "Driver seat not occupied\r\n", 26);
-        }
-        if ( sensorsOn[3] == 0) {
-            uartUsb.write( "Driver seatbelt not occupied\r\n", 30);
-        }
+    if (sensorsOn[1] == 0) {
+      uartUsb.write("Passenger seatbelt not fastened\r\n", 33);
     }
+    if (sensorsOn[2] == 0) {
+      uartUsb.write("Driver seat not occupied\r\n", 26);
+    }
+    if (sensorsOn[3] == 0) {
+      uartUsb.write("Driver seatbelt not fastened\r\n", 30);
+    }
+  }
 }
 
-bool areEqual()
-{
-    int i;
+bool areEqual() {
+  int i;
 
-    for (i = 0; i < NUMBER_OF_SENSORS; i++) {
-        if (allOn[i] != sensorsOn[i]) {
-            return false;
-        }
+  for (i = 0; i < NUMBER_OF_SENSORS; i++) {
+    if (allOn[i] != sensorsOn[i]) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
-
